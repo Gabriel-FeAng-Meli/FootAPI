5c5aa5cbea0b7b690dc0c239991007fac7b3f169
Gabriel-Feang-Meli <gabriel.fangelo@mercadolivre.com>
2025-01-08 18:16:21 -0300

    first

---
+++ src/main/java/com/meli/footapi/FootApiApplication.java
@@ -0,0 +1,13 @@
+package com.meli.footapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootApiApplication.class, args);
+    }
+
+}
